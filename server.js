459eaf6b7eade1fb50107b60390581a01be6db73
hollanM <h.mcclendon@eagles.oc.edu>
2026-01-30 15:51:14 -0600

    fixed router

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
   
 // Load the routes from the routes folder
-app.use("/tutorial", routes); 
+app.use("/workerscheduling-t4", routes); 
 
 
 // set port, listen for requests
