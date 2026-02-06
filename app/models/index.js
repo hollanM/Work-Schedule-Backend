e98ab93873f9592b8dd0c8d32f40f2139c39bd06
joolean-equals-true <Julian.Moore7970@gmail.com>
2026-02-05 20:27:42 -0600

    Department tables schedule is complete. To return two times for each day a nested query will need to be made with the date time table when this branch is merged.

--- app/models/index.js
+++ app/models/index.js
@@ -9,6 +9,11 @@ import Session from "./session.model.js";
 import Tutorial from "./tutorial.model.js";
 import Lesson from "./lesson.model.js"; 
 
+//Julian's department Model changes begin here.
+import Department_Schedule from "./department_schedule.model.js";
+//Julian's department Model changes end here.
+
+
 
 const db = {};
 db.Sequelize = Sequelize;
@@ -18,6 +23,10 @@ db.user = User;
 db.session = Session;
 db.tutorial = Tutorial;
 db.lesson = Lesson;
+
+//Julian's department Model changes begin here.
+db.department_schedule = Department_Schedule;
+//Julian's department Model changes end here.
 
 // foreign key for session
 db.user.hasMany(
@@ -55,4 +64,7 @@ db.lesson.belongsTo(
   { foreignKey: { allowNull: false }, onDelete: "CASCADE" }
 );
 
+
+
+//Associations for Department_Schedules (to date time) needs to go here. 
 export default db;
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,10 @@ import UserRoutes from "./user.routes.js";
 import TutorialRoutes from "./tutorial.routes.js";
 import LessonRoutes from "./lesson.routes.js";
 
+//Julian's department routes changes start here.
+import DepartmentScheduleRoutes from "./department_schedule.routes.js";
+//Julian's department routes changes end here.
+
 
 const router = Router();
 
@@ -12,5 +16,9 @@ router.use("/", AuthRoutes);
 router.use("/users", UserRoutes);
 router.use("/tutorials", TutorialRoutes);
 router.use("/tutorials", LessonRoutes);
+
+//Julian's department routes changes start here.
+router.use("/department_schedules", DepartmentScheduleRoutes);
+//Julian's department routes changes end here.
 
 export default router;
